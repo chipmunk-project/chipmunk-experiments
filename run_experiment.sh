d5dbcc38bac4b89183f290eef3dcec98ff4869e9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-02-23 12:23:01 -0500

    Add serial_or_parallel argument to run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,11 +4,12 @@ alu=${2?Input alu}
 pipeline_len=${3?Input pipeline_len}
 pipeline_width=${4?Input pipeline_width}
 chipmunk_folder=${5?Input chipmunk_folder}
+serial_or_parallel=${6?Input serial_or_parallel}
 
 domino_to_chipmunk $domino_program > /tmp/tmp.sk
 
 cd $chipmunk_folder
-Output_result=$(python3 chipmunk.py /tmp/tmp.sk $alu $pipeline_len $pipeline_width codegen sample1)
+Output_result=$(python3 chipmunk.py /tmp/tmp.sk $alu $pipeline_len $pipeline_width codegen sample1 $serial_or_parallel)
 
 echo -e "\n\nCompilation result:"
 if [[ $Output_result == *"Sketch succeeded"* ]]; then
