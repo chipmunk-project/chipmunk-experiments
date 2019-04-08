693c9a842ace591ce44558312c827161631617f2
Xiangyu Gao <xg673@nyu.edu>
2019-04-07 21:36:29 -0400

    Update the automatical way to run whole process
    From domino.cc -> canonicalizer -> grouper
         -> domino_to_chipmunk .sk -> iterative_solver

--- compile_with_chipmunk.py
+++ compile_with_chipmunk.py
@@ -1,4 +1,4 @@
-#TODO: .c --- (canonocalizer) ---> canonicalizer_file.c --- (grouper)
+# domino.c --- (canonocalizer) ---> canonicalizer_file.c --- (grouper)
 #  ---> a_group_of_domino_files.cc --- (domino_to_chipmunk) ---> .sk files
 
 import sys
@@ -8,27 +8,38 @@ import subprocess
 
 def main(argv):
     """Main program."""
-    if len(argv) < 3:
-        print("Usage: python3 " + argv[0] + " <program file> <group size>")
+    if len(argv) != 9 :
+        print("Usage: python3 " + argv[0] + " <program file> <group size> <alu file> <number of pipeline stages> " +
+              "<number of stateless/stateful ALUs per stage> " +
+              "<sketch_name (w/o file extension)> <parallel/serial> " +
+              "<cex_mode/hole_elimination_mode>")
         exit(1)
     # program_file means the original domino program
     program_file = str(argv[1])
     group_size = str(argv[2])
+    alu_file = str(argv[3])
+    num_pipeline_stages = str(argv[4])
+    num_alus_per_stage = str(argv[5])
+    sketch_name = str(argv[6])
+    parallel_or_serial = str(argv[7])
+    mode = str(argv[8])
 
-    #TODO: run canonicalizer
+    # Run canonicalizer
     (ret_code,
      output) = subprocess.getstatusoutput("canonicalizer " + program_file)
-    #TODO: output canonicalizer file to /tmp/file_name.cc
+    # Output canonicalizer file to /tmp/file_name.cc
     canonicalizer_file = "/tmp/" + program_file[program_file.rfind('/') + 1:]
     with open(canonicalizer_file, 'w') as file:
         file.write(output)
-    #TODO: run grouper to generate files with different group strategies output to /tmp folder
+    # Run grouper to generate files with different group strategies output to /tmp folder
     # the name of group_files is /tmp/<file_name>_equivalent_(/d).c
     (ret_code,
      output) = subprocess.getstatusoutput("grouper " + canonicalizer_file +
                                           " " + group_size)
-    # output restore the total number of group files
-    for i in range(int(output)):
+    # Output restore the total number of group files
+    # the name of group_files is /tmp/<file_name>_equivalent_(/d).sk
+    total_num_of_grouped_files = int(output)
+    for i in range(total_num_of_grouped_files):
         group_file = "/tmp/" + program_file[program_file.rfind(
             '/') + 1:program_file.rfind('.')] + "_equivalent_" + str(i) + ".c"
         (ret_code, output) = subprocess.getstatusoutput("domino_to_chipmunk " +
@@ -36,7 +47,20 @@ def main(argv):
         chipmunk_file = group_file[:group_file.rfind('.')] + ".sk"
         with open(chipmunk_file, 'w') as file:
             file.write(output)
+    
+    # Run iterative solver for grouped files
+    for i in range(total_num_of_grouped_files):
+        sketch_file_name = "/tmp/" + program_file[program_file.rfind(
+            '/') + 1:program_file.rfind('.')] + "_equivalent_" + str(i) + ".sk"
+        # Print the current situation
+        print("Current compilation file is " + sketch_file_name)
+        # Get the string to run in terminal
+        str_to_run_in_terminal = "iterative_solver " + sketch_file_name + " " + alu_file + " " + num_pipeline_stages + " " + num_alus_per_stage + " " + sketch_name + " " + parallel_or_serial + " " + mode
+        (ret_code, _) = subprocess.getstatusoutput(str_to_run_in_terminal)
+        if (ret_code == 0):
+          print("Compilation succeeds")
+          sys.exit(0)
 
-
+    print("Compilation fails")
 if __name__ == "__main__":
     main(sys.argv)
