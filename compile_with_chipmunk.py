ff9ad7a8a89b446f5f8280e690183f54c548e548
XiangyuG <xg673@nyu.edu>
2019-06-04 05:31:42 -0400

    Update compile_with_chipmunk.py and expriment_results.txt

--- compile_with_chipmunk.py
+++ compile_with_chipmunk.py
@@ -1,5 +1,6 @@
 # domino.c --- (canonocalizer) ---> canonicalizer_file.c --- (grouper)
 #  ---> a_group_of_domino_files.cc --- (domino_to_chipmunk) ---> .sk files
+#  ---> run iterative_solver on each .sk file
 
 import sys
 import re
@@ -8,25 +9,26 @@ import subprocess
 
 def main(argv):
     """Main program."""
-    if len(argv) != 6 :
-        print("Usage: python3 " + argv[0] + " <domino program file> <group size> <alu file> <number of pipeline stages> " +
-              "<number of stateless/stateful ALUs per stage> ")
+    if len(argv) != 8 :
+        print("Usage: python3 " + argv[0] + " <domino program file> <group size> <stateful alu file> <stateless alu file> " +
+              "<number of pipeline stages> " +
+              "<number of stateless/stateful ALUs per stage> " +
+              "<input bits>")
         exit(1)
     # program_file means the original domino program
     program_file = str(argv[1])
     group_size = str(argv[2])
-    alu_file = str(argv[3])
-    num_pipeline_stages = str(argv[4])
-    num_alus_per_stage = str(argv[5])
-#    sketch_name = str(argv[6])
-#    parallel_or_serial = str(argv[7])
-#    mode = str(argv[8])
+    stateful_alu_file = str(argv[3])
+    stateless_alu_file = str(argv[4])
+    num_pipeline_stages = str(argv[5])
+    num_alus_per_stage = str(argv[6])
+    input_bits = str(argv[7])
 
     # Run canonicalizer
     (ret_code,
      output) = subprocess.getstatusoutput("canonicalizer " + program_file)
-    # Output canonicalizer file to /tmp/file_name.cc
-    canonicalizer_file = "/tmp/" + program_file[program_file.rfind('/') + 1:]
+    # Output canonicalizer file to /tmp/file_name_canonicalizer.c
+    canonicalizer_file ="/tmp/" + program_file[program_file.rfind('/') + 1 : program_file.rfind('.')] + "_canonicalizer.c"
     with open(canonicalizer_file, 'w') as file:
         file.write(output)
     # Run grouper to generate files with different group strategies output to /tmp folder
@@ -34,32 +36,35 @@ def main(argv):
     (ret_code,
      output) = subprocess.getstatusoutput("grouper " + canonicalizer_file +
                                           " " + group_size)
+    print(output)
     # Output restore the total number of group files
-    # the name of group_files is /tmp/<file_name>_equivalent_(/d).sk
+    # the name of group_files is /tmp/<file_name>_canonicalizer_equivalent_(/d).sk
     total_num_of_grouped_files = int(output)
     for i in range(total_num_of_grouped_files):
-        group_file = "/tmp/" + program_file[program_file.rfind(
-            '/') + 1:program_file.rfind('.')] + "_equivalent_" + str(i) + ".c"
+        group_file = "/tmp/" + canonicalizer_file[canonicalizer_file.rfind(
+            '/') + 1:canonicalizer_file.rfind('.')] + "_equivalent_" + str(i) + ".c"
         (ret_code, output) = subprocess.getstatusoutput("domino_to_chipmunk " +
                                                         group_file)
         chipmunk_file = group_file[:group_file.rfind('.')] + ".sk"
         with open(chipmunk_file, 'w') as file:
             file.write(output)
-    
     # Run iterative solver for grouped files
     for i in range(total_num_of_grouped_files):
-        sketch_file_name = "/tmp/" + program_file[program_file.rfind(
-            '/') + 1:program_file.rfind('.')] + "_equivalent_" + str(i) + ".sk"
+        sketch_file_name = "/tmp/" + canonicalizer_file[canonicalizer_file.rfind(
+            '/') + 1:canonicalizer_file.rfind('.')] + "_equivalent_" + str(i) +".sk"
         # Print the current situation
         print("Current compilation file is " + sketch_file_name)
         # Get the string to run in terminal
-        str_to_run_in_terminal = "iterative_solver " + sketch_file_name + " " + alu_file + " " + num_pipeline_stages + " " + num_alus_per_stage
+        str_to_run_in_terminal = "iterative_solver " + sketch_file_name + " " + stateful_alu_file + " " + stateless_alu_file + " " + \
+                                 num_pipeline_stages + " " + num_alus_per_stage + " " + input_bits + " --parallel-sketch"
         (ret_code, _) = subprocess.getstatusoutput(str_to_run_in_terminal)
+        # It will return 0 if one of the grouped files get successful compilation
         if (ret_code == 0):
-          print("Compilation succeeds for Program: " + program_file[program_file.rfind('/') + 1:] + ", with alu: " + alu_file + ", with grid size: " + num_pipeline_stages + " * " + num_alus_per_stage)
+          print("Compilation succeeds for Program: " + program_file[program_file.rfind('/') + 1:] + ", with stateful alu: " + stateful_alu_file + "and stateless alu: " + stateless_alu_file + ", with grid size: " + num_pipeline_stages + " * " + num_alus_per_stage)
           sys.exit(0)
 
-    print("Compilation fails for Program: " + program_file[program_file.rfind('/') + 1:] + ", with alu: " + alu_file + ", with grid size: " + num_pipeline_stages + " * " + num_alus_per_stage)
+    print("Compilation fails for Program: " + program_file[program_file.rfind('/') + 1:] + ", with alu: " + stateful_alu_file + "and stateless alu: " + stateless_alu_file + ", with grid size: " + num_pipeline_stages + " * " + num_alus_per_stage)
+    sys.exit(1)
 
 if __name__ == "__main__":
     main(sys.argv)
