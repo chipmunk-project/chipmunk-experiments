9c87dcae3fdbabcedef055af87a612d447236783
XiangyuG <xg673@nyu.edu>
2019-06-17 11:36:48 -0400

    Add marple_new_flow and marple_tcp_nmo files

---
+++ June_16_experiments/marple_tcp_nmo/3_3_pred_raw_stateless_alu/marple_tcp_nmo_equivalent_6_canonicalizer_equivalent_0.c
@@ -0,0 +1,26 @@
+struct Packet{
+    int pkt_0;
+};
+int state_group_1_state_0 = 0;
+int state_group_0_state_0 = 0;
+void func(struct Packet p){
+if (!(!(!(!(!(!(!(p.pkt_0<state_group_0_state_0)))))))&&1==1) {if (!(!(!(!(!(p.pkt_0<state_group_0_state_0)))))&&1==1) {if (!(!(!(p.pkt_0<state_group_0_state_0)))&&1==1) {if (!(p.pkt_0<state_group_0_state_0)&&1==1) {state_group_0_state_0=p.pkt_0;
+
+;}
+
+;}
+
+;}
+
+;}else {if (!(!(!(!(!(!(p.pkt_0<state_group_0_state_0))))))&&1==1) {if (!(!(!(!(p.pkt_0<state_group_0_state_0))))&&1==1) {if (!(!(p.pkt_0<state_group_0_state_0))&&1==1) {if (p.pkt_0<state_group_0_state_0&&1==1) {state_group_1_state_0=state_group_1_state_0+1;
+
+;}
+
+;}
+
+;}
+
+;}
+
+;}
+}
