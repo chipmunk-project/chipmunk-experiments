c2d39eef3dcf6cb29fd8d87ee11e35972cbaafad
XiangyuG <xg673@nyu.edu>
2019-06-20 22:08:09 -0400

    Add related files for flowlets, marple_new_flow and stateful_fw

---
+++ June_19_experiments/marple_new_flow/4_4_pred_raw_stateless_alu_arith/marple_new_flow_equivalent_6_canonicalizer.c
@@ -0,0 +1,23 @@
+/* 
+// Original program: 
+int count = 0;
+struct Packet {
+    int new;
+};
+void func(struct Packet p){
+if (count==0 && 1==1) {count=1;
+p.new=1;
+
+ }
+} */
+
+int state_0 = 0;
+struct Packet{
+    int pkt_0;
+};
+void func(struct Packet p) {
+if (state_0==0&&1==1) {state_0=1;
+p.pkt_0=1;
+;}
+
+}
