745c3c5d123e30cc1ab58d828608b6dd0e69da1e
XiangyuG <xg673@nyu.edu>
2019-06-26 10:48:33 -0400

    Add output files for June 26th experiments

---
+++ June_26_experiments/sampling/2_1_pred_raw_stateless_alu_arith/sampling_equivalent_9_canonicalizer_equivalent_0.c
@@ -0,0 +1,15 @@
+struct Packet{
+    int pkt_0;
+};
+int state_group_0_state_0 = 0;
+void func(struct Packet p){
+if (1==1&&!(state_group_0_state_0==3-1&&1==1)&&1==1&&1==1) {p.pkt_0=0;
+state_group_0_state_0=state_group_0_state_0+1;
+
+;}else {if (1==1&&1==1&&state_group_0_state_0==3-1&&1==1&&1==1) {p.pkt_0=1;
+state_group_0_state_0=0;
+
+;}
+
+;}
+}
