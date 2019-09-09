5a33a139c54a32373a2db4b2efe84e2babbd558f
Ubuntu <ubuntu@ip-172-31-44-213.us-east-2.compute.internal>
2019-09-09 02:18:40 +0000

    Add flowlets output files

---
+++ Aug_26_experiments/flowlets/flowlets_equivalent_8.c
@@ -0,0 +1,16 @@
+struct Packet{
+int sport;
+int dport;
+int new_hop;
+int arrival;
+int next_hop;
+int id;
+};
+int last_time[8000] = {0};
+int saved_hop[8000] = {0};
+void flowlet(struct Packet pkt){
+if (1==1&&pkt.arrival-last_time[pkt.id]>5&&1==1) {saved_hop[pkt.id]=pkt.new_hop;
+;}
+last_time[pkt.id]=pkt.arrival;
+pkt.next_hop=saved_hop[pkt.id];
+}
